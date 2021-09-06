14e69498bb557455767f46f73237e230496f47e3
TeodorCa <cazangiu.teodor@gmail.com>
2021-09-07 00:37:05 +0300

    Updates

---
+++ precache-manifest.707b67de3087b57d02e5cc8b9584dc0d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b744078180fb1d1ddd7a3e439d5f8860",
+    "url": "/shop-mayusha/index.html"
+  },
+  {
+    "revision": "39a01414b49010c56b35",
+    "url": "/shop-mayusha/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "49fb7892da9b766f894c",
+    "url": "/shop-mayusha/static/css/main.726eb8aa.chunk.css"
+  },
+  {
+    "revision": "39a01414b49010c56b35",
+    "url": "/shop-mayusha/static/js/2.6444a545.chunk.js"
+  },
+  {
+    "revision": "170392c3acad6bbffec743442b1d0ac3",
+    "url": "/shop-mayusha/static/js/2.6444a545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49fb7892da9b766f894c",
+    "url": "/shop-mayusha/static/js/main.3162bff5.chunk.js"
+  },
+  {
+    "revision": "9b0e79caf76e2db3b870",
+    "url": "/shop-mayusha/static/js/runtime-main.50ffb665.js"
+  },
+  {
+    "revision": "648cc3b4ec15006207a91af8a916e26d",
+    "url": "/shop-mayusha/static/media/github.648cc3b4.svg"
+  },
+  {
+    "revision": "173ca344d0b68783bf9f69f2e554e5cc",
+    "url": "/shop-mayusha/static/media/linkedin.173ca344.svg"
+  },
+  {
+    "revision": "aea4cb716fcb7ff1f0ba5e077dd8f419",
+    "url": "/shop-mayusha/static/media/logo.aea4cb71.png"
+  },
+  {
+    "revision": "c4d17416856a8e299c17aef22680b139",
+    "url": "/shop-mayusha/static/media/mail.c4d17416.svg"
+  },
+  {
+    "revision": "802cd03a0acab046d41deccc02c90a87",
+    "url": "/shop-mayusha/static/media/phone.802cd03a.svg"
+  },
+  {
+    "revision": "4eb6a9305e0b9b55be802ffa8bfc56c3",
+    "url": "/shop-mayusha/static/media/shopping-cart.4eb6a930.svg"
+  }
+]);
