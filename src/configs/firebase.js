31cbbe1a2fe7e894ac3f7441047fc14a80ed3ff2
TeodorCa <cazangiu.teodor@gmail.com>
2021-12-19 18:04:33 +0200

    resolved path problem for firebase

---
+++ src/configs/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAAqbNDoKGJqMOwoHr0O_fiZ6gRoOHX4P8",
+    authDomain: "shop-mayusha.firebaseapp.com",
+    databaseURL: "https://shop-mayusha.firebaseio.com",
+    projectId: "shop-mayusha",
+    storageBucket: "shop-mayusha.appspot.com",
+    messagingSenderId: "573983152495",
+    appId: "1:573983152495:web:aa81c64412fcc39d0ab09a",
+    measurementId: "G-31Q0LBDJYQ"
+  };
+
+  export default firebaseConfig;
